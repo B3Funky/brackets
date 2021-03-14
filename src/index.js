a1088bab1eaca593b1bc8e5559e9634e1be408c6
B3Funky <tyutin.s@list.ru>
2021-03-14 16:26:58 +0300

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let arr = str.split('');
+  let arrLength = arr.length;
+
+  return arrSearch(arr, bracketsConfig);
+
+  function arrSearch(arr, bracketsConfig) {
+      let done;
+      for (let i = 0; i < bracketsConfig.length; i++) {
+          done = findCloseBrackets(arr, bracketsConfig[i][0], bracketsConfig[i][1]);
+      }
+      if (
+          typeof done !== "boolean"
+          &&
+          arrLength !== arr.length
+      ) {
+          arrLength = arr.length;
+          return arrSearch(arr, bracketsConfig)
+      }
+      return done
+  }
+
+  function findCloseBrackets(arr, openBracket, closeBracket) {
+      if (!arr.length) {
+          return true
+      }
+      for (let i = 0; i < arr.length; i++) {
+          if (arr[i] === openBracket && arr[i + 1] === closeBracket) {
+              arr.splice(i, 2);
+              return findCloseBrackets(arr, openBracket, closeBracket)
+          }
+      }
+      if (
+          Array.isArray(arr)
+          &&
+          arrLength !== arr.length
+      ) {
+          return arr
+      }
+      return false
+  }
+};
